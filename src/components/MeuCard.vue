17b1ea515113c086b96f4ab1237e1e3206373443
jeanpaullopes <jean.lopes@ulife.com.br>
2023-10-30 15:24:56 -0300

    primeiro commit

---
+++ src/components/MeuCard.vue
@@ -0,0 +1,46 @@
+<template>
+  <q-card bordered
+  class="q-pa-md">
+  <h4 v-show="cont%2 == 0">{{ cont }}</h4>
+  <h2>{{ mensagem }}</h2>
+  data: {{ todo.data }}<br/>
+  titulo: <h3>{{ todo.titulo }}</h3><br/>
+  <q-btn label="botão" outline class="bg-green"
+  @click="quandoClicar"/><br/>
+  <input v-model="mensagem" type="text" class="q-pa-md"/><br/>
+  <input v-model="mensagem" type="text" class="q-pa-md"/><br/>
+
+</q-card>
+</template>
+<script>
+export default {
+  name: "MeuCard",
+  props: {
+    texto: String,
+    init: Number,
+    timeout: Number,
+    todo: {
+      type: Object,
+      required: false,
+    },
+  },
+  data() {
+    return {
+      mensagem: "bem vindo ao Vue.js App",
+      cont:1
+    };
+  },
+  created() {
+    this.cont = this.init;
+    this.mensagem = this.texto;
+    setInterval(() => {
+      this.cont++;
+    }, this.timeout);
+  },
+  methods: {
+    quandoClicar() {
+      alert(this.todo.titulo);
+    },
+  },
+}
+</script>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,39 @@
+<template>
+  <q-page class="flex flex-center">
+    <MeuCard texto="da sexta"
+    :todo="todoObj" init="3000" timeout="500"/>
+    <MeuCard texto="o card 2" :init="1000" :timeout="1000"
+    :todo="todo2"/>
+
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import MeuCard from '../components/MeuCard.vue';
+export default defineComponent(
+  {
+    name: 'IndexPage',
+    components: {MeuCard},
+    data() {
+        return {
+            msg: 'bem vindo ao Vue.js App',
+            todoObj: Object,
+            todo2: Object
+        };
+    },
+    created() {
+      this.todoObj = {
+                data: '01/01/2021',
+                titulo: 'meu todo',
+            };
+      this.todo2 = {
+          data: '01/01/2021',
+          titulo: 'este é o todo 2',
+      };
+        console.log('IndexPage created');
+    },
+
+}
+)
+</script>
